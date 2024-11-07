7dac30e01b2e0a0ac1999a6a1c1e8198146f9f1a
Rasmus Anthin <scratty@gmail.com>
2024-11-07 20:51:02 +0100

    christmas_demo.cpp: * More fluff.

--- Christmas_Demo/christmas_demo.cpp
+++ Christmas_Demo/christmas_demo.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  christmas_demo.cpp
 //  Christmas_Demo
 //
 //  Created by Rasmus Anthin on 2024-11-07.
@@ -7,7 +7,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
   // insert code here...
   std::cout << "Hello, World!\n";
   return 0;
