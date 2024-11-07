d95aaf62501bd1a3c405ed645061d294b9fa53f6
Rasmus Anthin <scratty@gmail.com>
2024-11-07 20:49:41 +0100

    Adding main.cpp.

---
+++ Christmas_Demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Christmas_Demo
+//
+//  Created by Rasmus Anthin on 2024-11-07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
