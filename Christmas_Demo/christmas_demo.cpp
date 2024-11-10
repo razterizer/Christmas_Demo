e8969913ea8804540f840334f0e6c0153f69e433
Rasmus Anthin <scratty@gmail.com>
2024-11-10 10:08:39 +0100

    Setting the coeff of restitution for the snowflake to 0 again.

--- Christmas_Demo/christmas_demo.cpp
+++ Christmas_Demo/christmas_demo.cpp
@@ -166,7 +166,8 @@ public:
     sprite_snowflake->set_sprite_bg_colors(0, Color::Transparent2);
     sprite_snowflake->set_sprite_materials(0, 1);
     
-    rb_snowflake = dyn_sys.add_rigid_body(sprite_snowflake, .5f, { 0.5f, -3.f }, { 0.1f, 0.12f }, 0.f, 0.f, 0.1f);
+    float e = 0.f;
+    rb_snowflake = dyn_sys.add_rigid_body(sprite_snowflake, .5f, { 0.5f, -3.f }, { 0.1f, 0.12f }, 0.f, 0.f, e);
     
     
     for (int s_idx = 0; s_idx < 30; ++s_idx)
