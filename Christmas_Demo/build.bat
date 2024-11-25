71873d214e1e11745e5094a726dbfbcbc6c25b3d
Rasmus Anthin <scratty@gmail.com>
2024-11-25 19:24:09 +0100

    Fixed Windows build.

--- Christmas_Demo/build.bat
+++ Christmas_Demo/build.bat
@@ -5,7 +5,7 @@ SET configuration="Release"
 IF "%~1" == "Debug" SET configuration="Debug"
 SET target="x64"
 IF "%~2" == "x86" SET target="x86"
-msbuild Christmas_Demo.vs\Christmas_Demo.vs.sln /p:Configuration=%configuration% /p:Platform=%target%
+msbuild Christmas_Demo.vs.sln /p:Configuration=%configuration% /p:Platform=%target%
 
 if %errorlevel% neq 0 (
     echo Compilation failed with error code %errorlevel%.
