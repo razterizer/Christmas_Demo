624121fa2fc6e9fd29b48289e613290ce9241252
Rasmus Anthin <scratty@gmail.com>
2025-09-22 08:38:02 +0200

    setup_and_build_debian.sh: Installing ALSA dev packages.

--- setup_and_build_debian.sh
+++ setup_and_build_debian.sh
@@ -4,6 +4,8 @@ REPO_DIR="Christmas_Demo"
 
 # sudo apt install libopenal-dev
 
+sudo apt install libasound2-dev pkg-config
+
 cd ..
 
 ./"${REPO_DIR}"/fetch-dependencies.py "${REPO_DIR}"/dependencies
